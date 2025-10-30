cfff12fc30041840dd2ffeee02c36585f663703a
Don1L <danilic206@gmail.com>
2025-10-30 07:41:44 +0300

    Create Topic entity

--- WebForumApiApp/Models/Topic.cs
+++ WebForumApiApp/Models/Topic.cs
@@ -2,5 +2,12 @@ namespace WebForumApiApp.Models;
 
 public class Topic
 {
+    public int Id { get; set; }
     
+    public string Name { get; set; }
+    
+    public string? Description { get; set; }
+    
+    //Навигация
+    public ICollection<Thread> Threads { get; set; } = new List<Thread>();
 }
