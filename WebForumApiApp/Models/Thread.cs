300817212d8bb2cf6b12e90d3d15035b10534fc4
Don1L <danilic206@gmail.com>
2025-10-30 07:47:43 +0300

    Create Thread entity

---
+++ WebForumApiApp/Models/Thread.cs
@@ -0,0 +1,29 @@
+namespace WebForumApiApp.Models;
+
+public class Thread
+{
+    public int Id { get; set; }
+    
+    public string Title { get; set; } = string.Empty;
+    
+    public string Text { get; set; } = string.Empty;
+    
+    public string? PhotoPath { get; set; }
+    
+    public int AuthorId { get; set; }
+    
+    public int TopicId { get; set; }
+    
+    public DateTime CreatedOn { get; set; }
+    
+    public bool IsDeleted { get; set; }
+
+    public bool IsHidden { get; set; }
+    
+    // Навигация
+    public User Author { get; set; } = null!;
+    public Topic Topic { get; set; } = null!;
+
+    public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    public ICollection<ThreadTag> ThreadTags { get; set; } = new ICollection<ThreadTag>();
+}
