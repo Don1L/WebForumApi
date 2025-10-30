c555bc908eb2a7840790082c4fa2a13b94448452
Don1L <danilic206@gmail.com>
2025-10-30 07:52:22 +0300

    Create Tag entity

--- WebForumApiApp/Models/Tag.cs
+++ WebForumApiApp/Models/Tag.cs
@@ -2,5 +2,10 @@ namespace WebForumApiApp.Models;
 
 public class Tag
 {
+    public int Id { get; set; }
     
+    public string Name { get; set; } = string.Empty;
+    
+    // Навигация
+    public ICollection<ThreadTag> ThreadTags { get; set; } = new List<ThreadTag>();
 }
