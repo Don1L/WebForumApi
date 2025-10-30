69b12dfcc53ba8ebbe629d78062461f9c65bb741
Don1L <danilic206@gmail.com>
2025-10-30 07:31:38 +0300

    Create User entity

---
+++ WebForumApiApp/Models/User.cs
@@ -0,0 +1,16 @@
+namespace WebForumApiApp.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    
+    public string Username { get; set; }
+    
+    public string? PhotoPath { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string PasswordHash { get; set; }
+
+    public bool IsDeleted { get; set; }
+}
