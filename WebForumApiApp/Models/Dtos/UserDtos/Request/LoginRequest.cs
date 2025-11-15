0ae3c002bcd8c8bebcf01f4bfbd33a7443a194cb
Don1L <danilic206@gmail.com>
2025-11-15 10:55:34 +0300

    Update User DTO

--- WebForumApiApp/Models/Dtos/UserDtos/Request/LoginRequest.cs
+++ WebForumApiApp/Models/Dtos/UserDtos/Request/LoginRequest.cs
@@ -3,6 +3,6 @@ using System.ComponentModel.DataAnnotations;
 namespace WebForumApiApp.Models.Dtos.UserDtos.Request;
 
 public record LoginRequest(
-    [Required]string Username,
-    [Required]string Password
+    [Required][StringLength(50, MinimumLength = 3)]string Username,
+    [Required][StringLength(100, MinimumLength = 8)]string Password
     );
--- WebForumApiApp/Models/Dtos/UserDtos/Response/AuthResponse.cs
+++ WebForumApiApp/Models/Dtos/UserDtos/Response/AuthResponse.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 namespace WebForumApiApp.Models.Dtos.UserDtos.Response;
 
 public record AuthResponse(
-[Required] string Token,
+    [Required] string Token,
     UserResponse User,
     DateTime ExpiresAt
     );
